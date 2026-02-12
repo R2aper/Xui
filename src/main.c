75c285330c35bc4e5ac1efb8e5ef6da87836a9db
Mihail <Mihailzharikow@yandex.ru>
2026-02-12 22:31:25 +0700

    feat: Add application start to the main function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
+#include "defer.h"
+#include "model.h"
+#include "tui.h"
+#include <assert.h>
+
+#include <notcurses/notcurses.h>
 
 int main(void) {
+  struct notcurses_options opts = {
+      .flags = NCOPTION_NO_CLEAR_BITMAPS | NCOPTION_PRESERVE_CURSOR,
+      .loglevel = NCLOGLEVEL_WARNING,
+  };
+  model_t state = model_t_init(opts);
+  assert(state.nc);
 
-  puts("Hello from xui!");
+  defer { model_t_cleanup(&state); };
+
+  assert(run_app(&state) != 0);
 
   return 0;
 }
