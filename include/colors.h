4fd0219cf92672ba4eadc43ea7033b43c4db36e4
Mihail <Mihailzharikow@yandex.ru>
2026-02-12 22:26:10 +0700

    feat(tui/colors): add header file with used colors

---
+++ include/colors.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define DARK_BLUE 0x34495e       // #34495e
+#define MOUNTAIN_MEADOW 0x16a085 // #16a085
+#define WHITE 0xffffff           // #ffffff
+#define BLUE 0x3498db            // #3498db
+#define GREY 0x7f8c8d            // #7f8c8d
+#define RED 0xe74c3c             // #e74c3c
