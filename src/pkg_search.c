fa9734d1a16bdd26817d7238aae876576e6e3842
Mihail <Mihailzharikow@yandex.ru>
2026-02-17 21:32:54 +0700

    feat(pkg_search): implement package search using libxbps
    This commit introduces a new package search functionality that allows
    searching for packages in both local and remote repositories using the
    libxbps library

--- src/pkg_search.c
+++ src/pkg_search.c
@@ -193,32 +193,264 @@ static int remote_repo_callback(struct xbps_repo *repo, void *arg, bool *done) {
   return 0;
 }
 
-// TODO:
+/* ============= Search package ============= */
 
 search_result_t *search_packages(struct xbps_handle *xhp, const char *pattern,
                                  REPO_TYPE repo_type, bool use_regex) {
-  (void)xhp;
-  (void)pattern;
-  (void)repo_type;
-  (void)use_regex;
+  struct remote_search_context ctx;
+  search_result_t *results = malloc(sizeof(search_result_t));
+  if (!results)
+    return NULL;
 
-  return NULL;
+  memset(results, 0, sizeof(search_result_t));
+  results->packages = NULL;
+  results->count = 0;
+
+  ctx.base.pattern = pattern;
+  ctx.base.use_regex = use_regex;
+  ctx.base.results = results;
+  ctx.repo_uri = NULL;
+
+  if (use_regex) {
+    if (regcomp(&ctx.base.regexp, pattern,
+                REG_EXTENDED | REG_NOSUB | REG_ICASE) != 0) {
+      fprintf(stderr, "Failed to compile regex: %s\n", pattern);
+      return results;
+    }
+  }
+
+  if (repo_type == REMOTE)
+    xbps_rpool_foreach(xhp, remote_repo_callback, &ctx);
+  else if (repo_type == LOCAL)
+    xbps_pkgdb_foreach_cb(xhp, local_search_callback, &ctx.base);
+
+  if (use_regex)
+    regfree(&ctx.base.regexp);
+
+  return results;
 }
+
+/* ============= Get package metadata ============= */
 
 package_info_t *get_package_info(struct xbps_handle *xhp, const char *pkgname,
                                  REPO_TYPE repo_type) {
-  (void)xhp;
-  (void)pkgname;
-  (void)repo_type;
+  xbps_dictionary_t pkg_dict;
+  package_info_t *pkg;
 
-  return NULL;
+  if (repo_type == REMOTE) {
+    // Try to find pkg
+    pkg_dict = xbps_rpool_get_pkg(xhp, pkgname);
+
+    // If nothing is find, try to search for virtual pkg
+    if (!pkg_dict)
+      pkg_dict = xbps_rpool_get_virtualpkg(xhp, pkgname);
+
+    if (!pkg_dict)
+      return NULL;
+
+    pkg = malloc(sizeof(package_info_t));
+    if (!pkg)
+      return NULL;
+    memset(pkg, 0, sizeof(package_info_t));
+
+  } else if (repo_type == LOCAL) {
+    pkg_dict = xbps_pkgdb_get_pkg(xhp, pkgname);
+    if (!pkg_dict)
+      return NULL;
+
+    pkg = malloc(sizeof(package_info_t));
+    if (!pkg)
+      return NULL;
+    memset(pkg, 0, sizeof(package_info_t));
+  } else {
+    return NULL;
+  }
+
+  const char *value = NULL;
+
+  xbps_dictionary_get_cstring_nocopy(pkg_dict, "pkgver", &value);
+  if (value)
+    pkg->pkgver = strdup(value);
+
+  xbps_dictionary_get_cstring_nocopy(pkg_dict, "short_desc", &value);
+  if (value)
+    pkg->short_desc = strdup(value);
+
+  xbps_dictionary_get_cstring_nocopy(pkg_dict, "long_desc", &value);
+  if (value)
+    pkg->long_desc = strdup(value);
+
+  xbps_dictionary_get_cstring_nocopy(pkg_dict, "maintainer", &value);
+  if (value)
+    pkg->maintainer = strdup(value);
+
+  xbps_dictionary_get_cstring_nocopy(pkg_dict, "homepage", &value);
+  if (value)
+    pkg->homepage = strdup(value);
+
+  xbps_dictionary_get_cstring_nocopy(pkg_dict, "license", &value);
+  if (value)
+    pkg->license = strdup(value);
+
+  if (repo_type == LOCAL)
+    xbps_pkg_state_dictionary(pkg_dict, &pkg->state);
+
+  return pkg;
 }
+
+/* ============= Get package files  ============= */
 
 package_files_t *get_package_files(struct xbps_handle *xhp, const char *pkgname,
                                    REPO_TYPE repo_type) {
-  (void)xhp;
-  (void)pkgname;
-  (void)repo_type;
+  package_files_t *files = (package_files_t *)malloc(sizeof(package_files_t));
+  if (!files)
+    return NULL;
+  memset(files, 0, sizeof(package_files_t));
 
-  return NULL;
+  xbps_dictionary_t files_dict, file_obj;
+  xbps_array_t files_array;
+
+  if (repo_type == REMOTE) {
+    xbps_dictionary_t pkg_dict = xbps_rpool_get_pkg(xhp, pkgname);
+    if (!pkg_dict)
+      return NULL;
+
+    char bfile[PATH_MAX];
+    int rv = xbps_pkg_path_or_url(xhp, bfile, sizeof(bfile), pkg_dict);
+    if (rv < 0)
+      return NULL;
+
+    files_dict = xbps_archive_fetch_plist(bfile, "/files.plist");
+  } else {
+    files_dict = xbps_pkgdb_get_pkg_files(xhp, pkgname);
+  }
+
+  if (!files_dict)
+    return NULL;
+
+  // Ordinary files
+  files_array = xbps_dictionary_get(files_dict, "files");
+  if (files_array) {
+    for (uint32_t i = 0; i < xbps_array_count(files_array); i++) {
+      file_obj = xbps_array_get(files_array, i);
+      const char *filepath = NULL;
+      xbps_dictionary_get_cstring_nocopy(file_obj, "file", &filepath);
+      if (filepath) {
+        files->data = realloc(files->data, (files->count + 1) * sizeof(char *));
+        files->data[files->count++] = strdup(filepath);
+      }
+    }
+  }
+
+  // Config files
+  files_array = xbps_dictionary_get(files_dict, "conf_files");
+  if (files_array) {
+    for (unsigned int i = 0; i < xbps_array_count(files_array); i++) {
+      file_obj = xbps_array_get(files_array, i);
+      const char *filepath = NULL;
+      xbps_dictionary_get_cstring_nocopy(file_obj, "file", &filepath);
+      if (filepath) {
+        files->data = realloc(files->data, (files->count + 1) * sizeof(char *));
+        files->data[files->count++] = strdup(filepath);
+      }
+    }
+  }
+
+  // Links
+  files_array = xbps_dictionary_get(files_dict, "links");
+  if (files_array) {
+    for (unsigned int i = 0; i < xbps_array_count(files_array); i++) {
+      file_obj = xbps_array_get(files_array, i);
+      const char *filepath = NULL;
+      xbps_dictionary_get_cstring_nocopy(file_obj, "file", &filepath);
+      if (filepath) {
+        files->data = realloc(files->data, (files->count + 1) * sizeof(char *));
+        files->data[files->count++] = strdup(filepath);
+      }
+    }
+  }
+
+  xbps_object_release(files_dict);
+
+  return files;
+}
+
+/* ============= Cleanup functions ============= */
+
+void package_info_cleanup(package_info_t *pkg) {
+  if (!pkg)
+    return;
+
+  if (pkg->pkgver)
+    free(pkg->pkgver);
+
+  if (pkg->short_desc)
+    free(pkg->short_desc);
+
+  if (pkg->long_desc)
+    free(pkg->long_desc);
+
+  if (pkg->maintainer)
+    free(pkg->maintainer);
+
+  if (pkg->homepage)
+    free(pkg->homepage);
+
+  if (pkg->license)
+    free(pkg->license);
+
+  if (pkg->installed_size)
+    free(pkg->installed_size);
+
+  if (pkg->repository)
+    free(pkg->repository);
+
+  if (pkg)
+    free(pkg);
+}
+
+void search_result_cleanup(search_result_t *result) {
+  if (!result)
+    return;
+
+  for (unsigned int i = 0; i < result->count; i++) {
+    package_info_t *pkg = &result->packages[i];
+    if (pkg->pkgver)
+      free(pkg->pkgver);
+    if (pkg->short_desc)
+      free(pkg->short_desc);
+    if (pkg->long_desc)
+      free(pkg->long_desc);
+    if (pkg->maintainer)
+      free(pkg->maintainer);
+    if (pkg->homepage)
+      free(pkg->homepage);
+    if (pkg->license)
+      free(pkg->license);
+    if (pkg->installed_size)
+      free(pkg->installed_size);
+    if (pkg->repository)
+      free(pkg->repository);
+  }
+
+  if (result->packages)
+    free(result->packages);
+
+  if (result)
+    free(result);
+}
+
+void package_files_cleanup(package_files_t *files, unsigned int count) {
+  if (!files->data)
+    return;
+
+  for (uint32_t i = 0; i < count; i++) {
+    if (files->data[i])
+      free(files->data[i]);
+  }
+  if (files->data)
+    free(files->data);
+
+  if (files)
+    free(files);
 }
